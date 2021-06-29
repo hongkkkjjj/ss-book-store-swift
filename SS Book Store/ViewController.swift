0cf5c58a4f13a0a4da4d0e3275416420b0245397
jun hong <junhong@softspace.com.my>
2021-06-29 15:04:45 +0800

    Initial Commit

---
+++ SS Book Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SS Book Store
+//
+//  Created by Soft Space User on 29/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
